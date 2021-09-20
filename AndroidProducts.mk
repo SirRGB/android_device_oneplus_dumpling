8490ea9f74e0f8a291064052445a6740d861f8dc
SirRGB <liam_wagner@gmx.de>
2021-09-20 21:37:01 +0200

    change to Stellar OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/stellar_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    stellar_dumpling-user \
+    stellar_dumpling-userdebug \
+    stellar_dumpling-eng
