ae79376e2104e683bc8c2508e57903f581d1d362
SirRGB <liam_wagner@gmx.de>
2021-12-12 18:24:24 +0100

    dumpling: change sources to materium

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/materium_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    materium_dumpling-user \
+    materium_dumpling-userdebug \
+    materium_dumpling-eng
