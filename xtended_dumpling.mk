e06c900ff6839d77ffda75dfafca9236df5d7005
SirRGB <sirrgb@amyrom.ml>
2022-09-25 23:41:57 +0200

    dumpling: bringup xtended

--- xtended_dumpling.mk
+++ xtended_dumpling.mk
@@ -21,10 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_dumpling
+XTENDED_BUILD_MAINTAINER := SirRGB
+
+#  Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := xtended_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
